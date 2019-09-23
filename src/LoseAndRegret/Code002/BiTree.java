1f3fd364c601e2d0ee631c44ba48754a24a6e3a8
DanielDeen <ding.xuyong@gmail.com>
2019-09-24 01:05:19 +0800

    create package && add https request

---
+++ src/LoseAndRegret/Code002/BiTree.java
@@ -0,0 +1,10 @@
+package LoseAndRegret.Code002;
+
+/**
+ * @program Before2020
+ * @description: 二叉树的序列化与反序列化
+ * @author: ding
+ * @create: 2019/09/24 00:46
+ */
+public class BiTree {
+}
---
+++ src/MainCode/Main.md
@@ -0,0 +1,6 @@
+About LeetCode problems
+
+-----
+
+这个目录中主要是刷的LeetCode试题
+1. 
