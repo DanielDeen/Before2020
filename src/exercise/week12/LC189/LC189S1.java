b83230307807bb51f2705a53d4e8dded6eaf855c
DanielDeen <ding.xuyong@gmail.com>
2021-01-08 01:16:38 +0800

    week12

--- src/exercise/week12/LC189/LC189S1.java
+++ src/exercise/week12/LC189/LC189S1.java
@@ -1,7 +1,5 @@
 package exercise.week12.LC189;
 
-import java.util.Arrays;
-
 /**
  * @description: 189.旋转数组
  * @author: Daniel Deen
@@ -16,5 +14,23 @@ public class LC189S1 {
         System.arraycopy(nums, nums.length - step, newNums, 0, step);
         System.arraycopy(nums, 0, newNums, step, nums.length - step);
         System.arraycopy(newNums, 0, nums, 0, nums.length);
+    }
+
+    public void rotate1(int[] nums, int k) {
+        int step = k % nums.length;
+        if (step == 0) return;
+        reverse(nums, 0, nums.length);
+        reverse(nums, 0, k);
+        reverse(nums, k + 1, nums.length);
+    }
+
+    private void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
     }
 }
