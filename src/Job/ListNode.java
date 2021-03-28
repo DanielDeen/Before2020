4149bb514d55e66060027e1951e57c18c75176b0
DanielDeen <ding.xuyong@gmail.com>
2021-03-28 23:50:59 +0800

    common structure

---
+++ src/Job/ListNode.java
@@ -0,0 +1,24 @@
+package Job;
+
+/**
+ * @description: 链表
+ * @author: Daniel Deen
+ * @create: 2020-10-21 00:19
+ */
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/Job/TreeNode.java
@@ -0,0 +1,26 @@
+package Job;
+
+/**
+ * @description: tree node
+ * @author: Daniel Deen
+ * @create: 2021-03-28 23:33
+ */
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
