2735fbc22a1f52dae444e8edd03c5db52fdafe41
DanielDeen <ding.xuyong@gmail.com>
2022-02-23 09:20:41 +0800

    [0223]Java demo

---
+++ src/Micro/java/Animal.java
@@ -0,0 +1,17 @@
+package Micro.java;
+
+/**
+ * @program Before2020
+ * @description: 父类 Animal
+ * @author: ding
+ * @create: 2022/02/23 01:03
+ */
+public class Animal {
+	private String mao;
+
+	int age;
+
+	String name;
+
+
+}
---
+++ src/Micro/java/Cat.java
@@ -0,0 +1,15 @@
+package Micro.java;
+
+/**
+ * @program Before2020
+ * @description: 子类 Cat
+ * @author: ding
+ * @create: 2022/02/23 01:03
+ */
+public class Cat extends Animal{
+
+	public static void main(String[] args) {
+
+		Animal animal = new Cat();
+	}
+}
