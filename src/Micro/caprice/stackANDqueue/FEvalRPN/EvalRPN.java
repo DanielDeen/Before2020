be1c215306fedea8997bfb6be68495c2544c4904
DanielDeen <ding.xuyong@gmail.com>
2022-03-05 23:17:11 +0800

    [0305]Java stack&queue start

--- src/Micro/caprice/stackANDqueue/FEvalRPN/EvalRPN.java
+++ src/Micro/caprice/stackANDqueue/FEvalRPN/EvalRPN.java
@@ -1,5 +1,8 @@
 package Micro.caprice.stackANDqueue.FEvalRPN;
 
+import java.util.Objects;
+import java.util.Stack;
+
 /**
  * @program Before2020
  * @description: 150. 逆波兰表达式求值
@@ -8,11 +11,33 @@ package Micro.caprice.stackANDqueue.FEvalRPN;
  */
 public class EvalRPN {
 	public static void main(String[] args) {
-
+		System.out.println(new EvalRPN().evalRPN(new String[]{"4", "13", "5", "/", "+"}));
 	}
 
+	public int evalRPN(String[] tokens) {
+		Stack<Integer> stack = new Stack<>();
+		for (String token : tokens) {
+			if (isOperator(token)) {
+				int res = stack.pop();
+				if (Objects.equals(token, "+")) {
+					res += stack.pop();
+				} else if (Objects.equals(token, "-")) {
+					res -= stack.pop();
+				} else if (Objects.equals(token, "*")) {
+					res *= stack.pop();
+				} else if (Objects.equals(token, "/")) {
+					res /= stack.pop();
+				}
+				stack.push(res);
+			} else {
+				stack.push(Integer.parseInt(token));
+			}
+		}
 
-	public int evalRPN(String[] tokens){
+		return stack.pop();
+	}
 
+	public boolean isOperator(String str) {
+		return Objects.equals(str, "+") || Objects.equals(str, "-") || Objects.equals(str, "*") || Objects.equals(str, "/");
 	}
 }
