cc3ca4b9af116579ccd775c00183bee1b9de730d
DanielDeen <ding.xuyong@gmail.com>
2022-03-07 23:43:01 +0800

    [0305]Java stack&queue to end

--- src/Micro/caprice/stackANDqueue/HTopKElements/TopKFrequents.java
+++ src/Micro/caprice/stackANDqueue/HTopKElements/TopKFrequents.java
@@ -1,5 +1,7 @@
 package Micro.caprice.stackANDqueue.HTopKElements;
 
+import java.util.*;
+
 /**
  * @program Before2020
  * @description: 347. 前 K 个高频元素
@@ -12,7 +14,26 @@ public class TopKFrequents {
 	}
 
 	public int[] topKFrequent(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int n : nums) {
+			map.put(n, map.getOrDefault(n, 0) + 1);
+		}
 
+		Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
+		PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+		for (Map.Entry<Integer, Integer> entry : entries) {
+			queue.offer(entry);
+			if (queue.size() > k) {
+				queue.poll();
+			}
+		}
+
+		int[] res = new int[k];
+		for (int i = k - 1; i >= 0; i--) {
+			res[i] = Objects.requireNonNull(queue.poll()).getKey();
+		}
+
+		return res;
 	}
 
 }
