eb4387487f06a7332494b07df269e59fb10322d8
DanielDeen <ding.xuyong@gmail.com>
2022-03-20 00:00:59 +0800

    [0320]Java binarytree traversal -- LevelTraversal

--- src/Micro/caprice/binarytree/CLevelTraversal/LevelTraversal2.java
+++ src/Micro/caprice/binarytree/CLevelTraversal/LevelTraversal2.java
@@ -1,5 +1,7 @@
 package Micro.caprice.binarytree.CLevelTraversal;
 
+import java.util.*;
+
 /**
  * @program Before2020
  * @description: 107.二叉树的层序遍历2
@@ -7,4 +9,44 @@ package Micro.caprice.binarytree.CLevelTraversal;
  * @create: 2022/03/19 23:48
  */
 public class LevelTraversal2 {
+	List<List<Integer>> list = new ArrayList<>();
+
+	public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+		traversal(root);
+
+		Collections.reverse(list);
+		return list;
+	}
+
+	private void traversal(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(node);
+		while (!queue.isEmpty()) {
+			List<Integer> tmpList = new ArrayList<>();
+			int len = queue.size();
+			while (len > 0) {
+				TreeNode tmpNode = queue.poll();
+				tmpList.add(tmpNode.val);
+
+				if (tmpNode.left != null) {
+					queue.offer(tmpNode.left);
+				}
+				if (tmpNode.right != null) {
+					queue.offer(tmpNode.right);
+				}
+				len--;
+			}
+			list.add(tmpList);
+		}
+
+	}
+
+	private void traversal(TreeNode node, int deep) {
+
+	}
 }
