4287295be6e56defc0d6e8ad993dccb2f70e925d
DanielDeen <ding.xuyong@gmail.com>
2022-03-28 23:36:29 +0800

    [0328]Java binarytree traversal -- all path binarytree

---
+++ src/Micro/caprice/binarytree/IAllPathTree/BinaryTreePaths.java
@@ -0,0 +1,24 @@
+package Micro.caprice.binarytree.IAllPathTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program Before2020
+ * @description: 257. 二叉树的所有路径
+ * @author: ding
+ * @create: 2022/03/28 23:16
+ */
+public class BinaryTreePaths {
+	List<String> list = new ArrayList<>();
+	public List<String> binaryTreePaths(TreeNode root) {
+		if (root == null) {
+			List<String> path = new ArrayList<>();
+			path.add();
+			return list;
+		}
+
+
+
+	}
+}
