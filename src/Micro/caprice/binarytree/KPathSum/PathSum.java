c74636564f64497abed204cd88fd7aee500e9ff6
DanielDeen <ding.xuyong@gmail.com>
2022-03-30 23:31:40 +0800

    [0330]Java binarytree traversal -- path sum binarytree done

--- src/Micro/caprice/binarytree/KPathSum/PathSum.java
+++ src/Micro/caprice/binarytree/KPathSum/PathSum.java
@@ -1,5 +1,6 @@
 package Micro.caprice.binarytree.KPathSum;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,7 +10,34 @@ import java.util.List;
  * @create: 2022/03/30 22:23
  */
 public class PathSum {
-	public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+	List<List<Integer>> res = new ArrayList<>();
 
+	public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+		if (root == null) {
+			return res;
+		}
+		List<Integer> list = new ArrayList<>();
+		list.add(root.val);
+		traversal(root, targetSum-root.val, list);
+		return res;
+	}
+
+	private void traversal(TreeNode node, int count, List<Integer> list) {
+		if (node.left == null && node.right == null && count == 0) {
+			res.add(new ArrayList<>(list));
+			return;
+		}
+
+		if (node.left != null) {
+			list.add(node.left.val);
+			traversal(node.left, count - node.left.val, list);
+			list.remove(list.size() - 1);
+		}
+
+		if (node.right != null) {
+			list.add(node.right.val);
+			traversal(node.right, count - node.right.val, list);
+			list.remove(list.size() - 1);
+		}
 	}
 }
