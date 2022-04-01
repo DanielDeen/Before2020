7ba84f7e9a04063730db1d0bc5f943dfa8982e0e
DanielDeen <ding.xuyong@gmail.com>
2022-04-01 23:34:02 +0800

    [0401]Java binarytree traversal -- Construct binarytree

--- src/Micro/caprice/binarytree/LConstructBinaryTree/ConstructFromInAndPre.java
+++ src/Micro/caprice/binarytree/LConstructBinaryTree/ConstructFromInAndPre.java
@@ -8,6 +8,28 @@ package Micro.caprice.binarytree.LConstructBinaryTree;
  */
 public class ConstructFromInAndPre {
 	public TreeNode buildTree(int[] preorder, int[] inorder) {
-		return helper(0, 0, inorder.length - 1, preorder, inorder);
+		return helper(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+	}
+
+	private TreeNode helper(int[] preOrder, int preLeft, int preRight, int[] inOrder, int inLeft, int inRight) {
+		if (inLeft > inRight || preLeft > preRight) {
+			return null;
+		}
+
+
+		int idx = inLeft;
+		int val = preOrder[preLeft];
+		TreeNode root = new TreeNode(val);
+		for (int i = inLeft; i <= inRight; i++) {
+			if (inOrder[i] == val) {
+				idx = i;
+				break;
+			}
+		}
+
+		root.left = helper(preOrder, preLeft + 1, preLeft + idx - inLeft, inOrder, inLeft, idx - 1);
+		root.right = helper(preOrder, preLeft + idx - inLeft + 1, preRight, inOrder, idx + 1, inRight);
+
+		return root;
 	}
 }
