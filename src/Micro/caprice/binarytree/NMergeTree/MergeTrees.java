94f1e58240122fb43b6c139ff57838f9d83db281
DanielDeen <ding.xuyong@gmail.com>
2022-04-03 02:30:41 +0800

    [0403]Java binarytree traversal -- merge binarytree

---
+++ src/Micro/caprice/binarytree/NMergeTree/MergeTrees.java
@@ -0,0 +1,13 @@
+package Micro.caprice.binarytree.NMergeTree;
+
+/**
+ * @program Before2020
+ * @description: 617. 合并二叉树
+ * @author: ding
+ * @create: 2022/04/03 02:29
+ */
+public class MergeTrees {
+	public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+
+	}
+}
