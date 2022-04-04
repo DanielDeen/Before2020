e67f2174447428fe001c523eb2f4d123672bc175
DanielDeen <ding.xuyong@gmail.com>
2022-04-05 01:45:09 +0800

    [045]Java binarytree traversal -- search binarytree BST

--- src/Micro/caprice/binarytree/OSearchTree/IsValidBST.java
+++ src/Micro/caprice/binarytree/OSearchTree/IsValidBST.java
@@ -7,7 +7,23 @@ package Micro.caprice.binarytree.OSearchTree;
  * @create: 2022/04/04 00:03
  */
 public class IsValidBST {
+	TreeNode max;
 	public boolean isValidBST(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
 
+		boolean left = isValidBST(root.left);
+		if (!left) {
+			return  false;
+		}
+
+		if (max != null && root.val <= max.val) {
+			return false;
+		}
+
+		max = root;
+		boolean right = isValidBST(root.right);
+		return right;
 	}
 }
