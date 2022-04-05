721debf5c99d1bea3574497ab0aadb1e9a17430b
DanielDeen <ding.xuyong@gmail.com>
2022-04-06 07:43:33 +0800

    [0406]Java binarytree traversal -- valid binarytree BST

--- src/Micro/caprice/binarytree/OSearchTree/IsValidBST.java
+++ src/Micro/caprice/binarytree/OSearchTree/IsValidBST.java
@@ -1,5 +1,8 @@
 package Micro.caprice.binarytree.OSearchTree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @program Before2020
  * @description: 98. 验证二叉搜索树
@@ -25,5 +28,26 @@ public class IsValidBST {
 		max = root;
 		boolean right = isValidBST(root.right);
 		return right;
+	}
+
+	List<Integer> list;
+	private void traversal(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		traversal(root.left);
+		list.add(root.val);
+		traversal(root.right);
+	}
+	public boolean isValidBST1(TreeNode root) {
+		list = new ArrayList<>();
+		traversal(root);
+		for (int i = 1 ; i < list.size(); i++) {
+			// 主要要小于等于，搜索树里不能有相同元素
+			if (list.get(i) <= list.get(i-1)) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
