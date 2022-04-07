a63f36e3a6cf40cad82ccf48c8d660adf34c7e5a
DanielDeen <ding.xuyong@gmail.com>
2022-04-07 23:52:09 +0800

    [0407]Java binarytree traversal --  binarytree BST

--- src/Micro/caprice/binarytree/OSearchTree/ModeInTree.java
+++ src/Micro/caprice/binarytree/OSearchTree/ModeInTree.java
@@ -1,8 +1,6 @@
 package Micro.caprice.binarytree.OSearchTree;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @program Before2020
@@ -14,8 +12,30 @@ public class ModeInTree {
 	public int[] findMode(TreeNode root) {
 		Map<Integer, Integer> map = new HashMap<>();
 		traversal(root, map);
-		Collections.sort(map.entrySet(),(v1, v2)-> {
-			return v1-v2;
-		});
+		List<Integer> list = new ArrayList<>(map.values());
+		Collections.sort(list, (v1, v2) -> v2 - v1);
+		int MaxNode = list.get(0);
+		List<Integer> list1 = new ArrayList<>();
+		for (Integer k : map.keySet()) {
+			if (map.get(k) == MaxNode) {
+				list1.add(k);
+			}
+		}
+
+		int[] res = new int[list1.size()];
+		for (int i = 0; i < list1.size(); i++) {
+			res[i] = list1.get(i);
+		}
+		return res;
 	}
+
+	private void traversal(TreeNode node, Map<Integer, Integer> map) {
+		if (node == null) {
+			return;
+		}
+		map.put(node.val, map.getOrDefault(node.val, 0) + 1);
+		traversal(node.left, map);
+		traversal(node.right, map);
+	}
+
 }
