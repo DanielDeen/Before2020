68641e0e8a7a1bd5412863f4d8e4afe95417867b
DanielDeen <ding.xuyong@gmail.com>
2022-04-11 23:05:55 +0800

    [0411]Java binarytree traversal --  binarytree BST

--- src/Micro/caprice/binarytree/PCommonAncester/CommonAncestorInBST.java
+++ src/Micro/caprice/binarytree/PCommonAncester/CommonAncestorInBST.java
@@ -28,8 +28,27 @@ public class CommonAncestorInBST {
 	}
 
 
-
 	public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+		if (root.val > p.val && root.val > q.val) {
+			return lowestCommonAncestor(root.left, p, q);
+		}
+		if (root.val < p.val && root.val < q.val) {
+			return lowestCommonAncestor(root.right, p, q);
+		}
+		return root;
+	}
 
+
+	public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+		while (true) {
+			if (root.val > p.val && root.val > q.val) {
+				root = root.left;
+			} else if (root.val < p.val && root.val < q.val) {
+				root = root.right;
+			} else {
+				break;
+			}
+		}
+		return root;
 	}
 }
