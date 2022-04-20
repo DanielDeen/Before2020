d16c45f085b397cc56b81ba94d5986bc73771bec
DanielDeen <ding.xuyong@gmail.com>
2022-04-20 23:30:11 +0800

    [0420]Java binarytree traversal --  binarytree BST

---
+++ src/Micro/caprice/binarytree/OSearchTree/CovertToBST.java
@@ -0,0 +1,35 @@
+package Micro.caprice.binarytree.OSearchTree;
+
+import java.util.Arrays;
+
+/**
+ * @program Before2020
+ * @description: 108. 将有序数组转换为二叉搜索树
+ * @author: ding
+ * @create: 2022/04/20 23:07
+ */
+public class CovertToBST {
+	public static void main(String[] args) {
+		System.out.println(new CovertToBST().sortedArrayToBST(new int[]{-10, -3, 0, 5, 9}));
+	}
+
+
+	public TreeNode sortedArrayToBST(int[] nums) {
+		int len = nums.length;
+		int mid = len >> 1;
+
+		TreeNode root = new TreeNode(nums[mid]);
+		if (mid > 0) {
+			int[] leftArray = new int[mid];
+			System.arraycopy(nums, 0, leftArray, 0, leftArray.length);
+			root.left = sortedArrayToBST(leftArray);
+			if (mid + 1 < len) {
+				int[] rightArray = new int[len - mid - 1];
+				System.arraycopy(nums, mid + 1, rightArray, 0, rightArray.length);
+				root.right = sortedArrayToBST(rightArray);
+			}
+
+		}
+		return root;
+	}
+}
