321a793d0068da93259db212d8206cd346fe11f2
DanielDeen <ding.xuyong@gmail.com>
2022-04-25 23:51:41 +0800

    [0425]Java backtracking --  basic

--- src/Micro/caprice/backtracking/BCombinations/Combine.java
+++ src/Micro/caprice/backtracking/BCombinations/Combine.java
@@ -1,5 +1,7 @@
 package Micro.caprice.backtracking.BCombinations;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -9,7 +11,30 @@ import java.util.List;
  * @create: 2022/04/25 23:29
  */
 public class Combine {
-	public List<List<Integer>> combine(int n, int k) {
-
+	public static void main(String[] args) {
+		System.out.println(new Combine().combine(4, 2));
 	}
+
+
+	List<List<Integer>> ans;
+	public List<List<Integer>> combine(int n, int k) {
+		ans = new ArrayList<>();
+		backtracking(new LinkedList<>(), n, k, 1);
+		return ans;
+	}
+
+	private void backtracking(LinkedList<Integer> tmp, int n, int k, int flag) {
+		if (tmp.size() == k) {
+			ans.add(new ArrayList<>(tmp));
+			return;
+		}
+
+		for (int i = flag; i <= n; i++) {
+			tmp.add(i);
+			backtracking(tmp, n, k, ++flag);
+			tmp.removeLast();
+		}
+	}
+
+
 }
