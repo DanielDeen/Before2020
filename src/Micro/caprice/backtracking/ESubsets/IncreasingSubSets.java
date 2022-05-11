090dd4658aed360a28ad4ac205e09f986998d640
DanielDeen <ding.xuyong@gmail.com>
2022-05-12 00:15:30 +0800

    [0512]Java backtracking --  Subsets

---
+++ src/Micro/caprice/backtracking/ESubsets/IncreasingSubSets.java
@@ -0,0 +1,15 @@
+package Micro.caprice.backtracking.ESubsets;
+
+import java.util.List;
+
+/**
+ * @program Before2020
+ * @description: 491. 递增子序列
+ * @author: ding
+ * @create: 2022/05/12 00:14
+ */
+public class IncreasingSubSets {
+	public List<List<Integer>> findSubsequences(int[] nums) {
+
+	}
+}
--- src/Micro/caprice/backtracking/ESubsets/SubsetsII.java
+++ src/Micro/caprice/backtracking/ESubsets/SubsetsII.java
@@ -1,5 +1,7 @@
 package Micro.caprice.backtracking.ESubsets;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,7 +11,32 @@ import java.util.List;
  * @create: 2022/05/11 23:51
  */
 public class SubsetsII {
-	public List<List<Integer>> subsetsWithDup(int[] nums) {
+	public static void main(String[] args) {
+		System.out.println(new SubsetsII().subsetsWithDup(new int[]{1, 2, 2}));
+	}
 
+
+	List<List<Integer>> list = new ArrayList<>();
+
+	public List<List<Integer>> subsetsWithDup(int[] nums) {
+		Arrays.sort(nums);
+		backtracking(nums, new ArrayList<>(), 0);
+		return list;
+	}
+
+	private void backtracking(int[] nums, ArrayList<Integer> tmp, int index) {
+		if (index == nums.length) {
+			return;
+		}
+
+		list.add(new ArrayList<>(tmp));
+		for (int i = index; i < nums.length; i++) {
+			if (index < i && nums[i] == nums[i - 1]) {
+				continue;
+			}
+			tmp.add(nums[i]);
+			backtracking(nums, tmp, i + 1);
+			tmp.remove(tmp.size() - 1);
+		}
 	}
 }
