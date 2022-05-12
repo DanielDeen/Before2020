7aec39b1f7f1984b8e7a15e8c9da43cfb422c3c6
DanielDeen <ding.xuyong@gmail.com>
2022-05-12 23:42:49 +0800

    [0512]Java backtracking --  Subsets

--- src/Micro/caprice/backtracking/ESubsets/IncreasingSubSets.java
+++ src/Micro/caprice/backtracking/ESubsets/IncreasingSubSets.java
@@ -1,6 +1,9 @@
 package Micro.caprice.backtracking.ESubsets;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @program Before2020
@@ -9,7 +12,38 @@ import java.util.List;
  * @create: 2022/05/12 00:14
  */
 public class IncreasingSubSets {
+	public static void main(String[] args) {
+		System.out.println(new IncreasingSubSets().findSubsequences(new int[]{4, 6, 7, 7}));
+	}
+
+
+	List<List<Integer>> ans = new ArrayList<>();
+
 	public List<List<Integer>> findSubsequences(int[] nums) {
+		backtracking(nums, new ArrayList<>(), 0);
+		return ans;
+	}
+
+	public void backtracking(int[] nums, List<Integer> list, int index) {
+		if (list.size() > 1) {
+			ans.add(new ArrayList<>(list));
+		}
+
+		// 同一层，使用过的数字不能再被使用
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = index; i < nums.length; i++) {
+			if (list.size() > 0 && (list.get(list.size() - 1) > nums[i])) {
+				continue;
+			}
+			if (map.getOrDefault(nums[i], 0) >= 1) {
+				continue;
+			}
+
+			map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+			list.add(nums[i]);
+			backtracking(nums, list, i + 1);
+			list.remove(list.size() - 1);
+		}
 
 	}
 }
