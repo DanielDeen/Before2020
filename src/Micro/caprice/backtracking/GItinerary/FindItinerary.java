55261c4d768ce2c18dcc378df5c3feda2c13cd1f
DanielDeen <ding.xuyong@gmail.com>
2022-05-16 00:17:24 +0800

    [0516]Java backtracking --  Itinerary

---
+++ src/Micro/caprice/backtracking/GItinerary/FindItinerary.java
@@ -0,0 +1,15 @@
+package Micro.caprice.backtracking.GItinerary;
+
+import java.util.List;
+
+/**
+ * @program Before2020
+ * @description: 332. 重新安排行程
+ * @author: ding
+ * @create: 2022/05/15 22:43
+ */
+public class FindItinerary {
+	public List<String> findItinerary(List<List<String>> tickets) {
+
+	}
+}
